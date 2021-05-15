3791363e24c1840627229b9eba662627f5a14d4b
trongtinzsz <trongtin123pt@gmail.com>
2021-05-15 23:40:07 +0700

    ..

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -27,3 +27,27 @@ document.getElementById("customFile").addEventListener("change",()=>{
     f_name=f_name.replace(/C:\\fakepath\\/i, '');
     document.getElementById("custom-file-label").innerHTML=f_name;
 })
+
+/*========PAGINATION JAVASCRIPT ===========*/
+function pagedisplay(n) {
+    var x = document.querySelectorAll("#a1");
+    var y = document.querySelectorAll("#a2");
+    var t = document.querySelectorAll(".page-item");
+
+      if(n == 1) {
+          for(var i = 0; i<10;i++){
+              x[i].style.display = "block";
+              y[i].style.display = "none";                              
+          }
+          
+      }
+      else if(n == 2) {
+          for(var i = 0; i<10;i++){
+              x[i].style.display = "none";
+              y[i].style.display = "block";
+              
+          }
+
+      }
+    
+  }
